04ca6655cee475a965ffd49d09c82d9cca3d2e4f
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2022-06-23 16:04:01 -0400

    testing

--- app_cpp/app/main.cpp
+++ app_cpp/app/main.cpp
@@ -9,32 +9,45 @@
 #include <thread>
 #include <iostream>
 #include <limits>
+#include <semaphore.h>
 
 int test=0;
+long loop_counter = 0;
+float data_in_l[BLOCK_SIZE];
+float data_in_r[BLOCK_SIZE];
+sem_t audio_mutex;
+sem_t synth_mutex;
 
 void processNormalKeys(unsigned char key, int x, int y) {
     test = !test;    
 }
 
-void synth_thread(){
-    float data_in_l[BLOCK_SIZE];
-    float data_in_r[BLOCK_SIZE];
+void audio_thread(){
     start_pcm();
-    int j = 0;
     while(1){
-        j++;
+        sem_wait(&audio_mutex);
+        sem_post(&synth_mutex);
+        loop_counter += 1;
+        stream(data_in_l, data_in_r);
+    }
+    end_pcm();
+}
+
+void synth_thread(){
+    while(1){
+        sem_wait(&synth_mutex);
         for (int i=0; i<BLOCK_SIZE; i++){
             if (test){
-                data_in_l[i] = std::sin((2.0*PI/SAMPLE_RATE)*(((double)i + (double)j*BLOCK_SIZE)*261.63*std::pow(2.0, 4.0/12.0)));
-                data_in_r[i] = std::sin((2.0*PI/SAMPLE_RATE)*(((double)i + (double)j*BLOCK_SIZE)*261.63*std::pow(2.0, 0.0/12.0)));
+                data_in_l[i] = std::sin((2.0*PI/SAMPLE_RATE)*(((double)i + (double)loop_counter*BLOCK_SIZE)*261.63*std::pow(2.0, 4.0/12.0)));
+                data_in_r[i] = std::sin((2.0*PI/SAMPLE_RATE)*(((double)i + (double)loop_counter*BLOCK_SIZE)*261.63*std::pow(2.0, 0.0/12.0)));
             } else {
                 data_in_l[i] = 0;
                 data_in_r[i] = 0;
             }
         }
-        stream(data_in_l, data_in_r);
+        sem_post(&audio_mutex);
+
     }
-    end_pcm();
 }
 
 int main(int argc, char *argv[]){
@@ -43,17 +56,20 @@ int main(int argc, char *argv[]){
     //glutInitWindowSize (300,300);
     //glutCreateWindow ("OpenGL / C Example - Well House");
     //glutKeyboardFunc(processNormalKeys);
+    //
+    sem_init(&audio_mutex, 0, 0);
+    sem_init(&synth_mutex, 0, 1);
 
     std::thread synth_thread_inst = std::thread(synth_thread);
+    std::thread audio_thread_inst = std::thread(audio_thread);
 
-    for(int i=0; i<20; i++){
+    for(int j=0; j<20; j++){
         sleep(1);
-        printf("\n%d\n", i);
         test = !test;
-
     }
 
     synth_thread_inst.join();
+    audio_thread_inst.join();
 
     //glutMainLoop();
     //for (int j=0; j<3*100; j++){
